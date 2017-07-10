c11a7a0714a290e84b85f598435110749819ba9f
stsiushkevich <stsiushkevich@scnsoft.com>
2017-07-10 10:59:11 +0300

    reference

--- src/main/java/com/learn/reactjs/config/AppMvcConfiguration.java
+++ src/main/java/com/learn/reactjs/config/AppMvcConfiguration.java
@@ -3,8 +3,10 @@ package com.learn.reactjs.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
@@ -34,5 +36,12 @@ public class AppMvcConfiguration extends WebMvcConfigurerAdapter {
         registry
                 .addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/home");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
     }
 }
--- src/main/java/com/learn/reactjs/web/controllers/MainController.java
+++ src/main/java/com/learn/reactjs/web/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.learn.reactjs.web.controllers;
 import com.learn.reactjs.web.constants.Pages;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
@@ -10,16 +11,29 @@ import java.util.Map;
  * @author stsiushkevich
  */
 @Controller
+@RequestMapping("/")
 public class MainController {
-    @GetMapping ("/home")
+    @GetMapping("home")
     public String home(Map<String, Object> model) {
         model.put("currentPage", Pages.HOME);
         return "homeView";
     }
 
-    @GetMapping ("/about-project")
+    @GetMapping("about-project")
     public String aboutProject(Map<String, Object> model) {
         model.put("currentPage", Pages.ABOUT_PROJECT);
         return "aboutProjectView";
+    }
+
+    @GetMapping("terms-of-use")
+    public String termsOfUse(Map<String, Object> model) {
+        model.put("currentPage", Pages.TERMS_OF_USE);
+        return "termsOfUseView";
+    }
+
+    @GetMapping("contacts")
+    public String contacts(Map<String, Object> model) {
+        model.put("currentPage", Pages.CONTACTS);
+        return "contactsView";
     }
 }
