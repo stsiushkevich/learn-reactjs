4509f34ed4ce31231b343efb7a63487ebc36395a
stsiushkevich <stsiushkevich@scnsoft.com>
2017-08-16 15:31:52 +0300

    jsessionid was been deleted from url

--- src/main/java/com/learn/reactjs/config/ServletInitializer.java
+++ src/main/java/com/learn/reactjs/config/ServletInitializer.java
@@ -3,10 +3,22 @@ package com.learn.reactjs.config;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionTrackingMode;
+import java.util.HashSet;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AppConfiguration.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppConfiguration.class);
+    }
+
+    @Override
+    public void onStartup(ServletContext context) throws ServletException {
+        HashSet<SessionTrackingMode> set = new HashSet<SessionTrackingMode>();
+        set.add(SessionTrackingMode.COOKIE);
+        context.setSessionTrackingModes(set);
+    }
 }
