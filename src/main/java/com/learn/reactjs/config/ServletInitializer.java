66538ae94c9194cd3305b85bd684c91362dda879
stsiushkevich <stsiushkevich@scnsoft.com>
2017-08-18 11:35:06 +0300

    removing jsessionid from url

--- src/main/java/com/learn/reactjs/config/ServletInitializer.java
+++ src/main/java/com/learn/reactjs/config/ServletInitializer.java
@@ -3,7 +3,31 @@ package com.learn.reactjs.config;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import java.util.Collections;
+
 public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // This can be done here or as the last step in the method
+        // Doing it in this order will initialize the Spring
+        // Framework first, doing it as last step will initialize
+        // the Spring Framework after the Servlet configuration is
+        // established
+        super.onStartup(servletContext);
+
+        // This will set to use COOKIE only
+        servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
+        // This will prevent any JS on the page from accessing the
+        // cookie - it will only be used/accessed by the HTTP transport
+        // mechanism in use
+        SessionCookieConfig sessionCookieConfig = servletContext.getSessionCookieConfig();
+        sessionCookieConfig.setHttpOnly(true);
+    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
