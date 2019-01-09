748aa26d55c87049033284dc9eb9d06cca6779d8
stsiushkevich <warsplint@list.ru>
2019-01-10 00:56:52 +0300

    Редизайн сайта: фикс стилей на страницах

--- src/main/java/com/learn/reactjs/web/controllers/TutorialController.java
+++ src/main/java/com/learn/reactjs/web/controllers/TutorialController.java
@@ -17,13 +17,13 @@ public class TutorialController {
 
     @GetMapping
     public String tutorial(Map<String, Object> model) {
-        model.put("currentSection", Pages.TUTORIAL);
+        model.put("currentPage", Pages.TUTORIAL);
         return "tutorialView";
     }
 
     @GetMapping("/sections")
     public String topics(Map<String, Object> model) {
-        model.put("currentSection", Pages.TUTORIAL_SECTIONS);
+        model.put("currentPage", Pages.TUTORIAL_SECTIONS);
         return "basicsTopicsView";
     }
 }
--- src/main/webapp/WEB-INF/pages/tiles/footer.jsp
+++ src/main/webapp/WEB-INF/pages/tiles/footer.jsp
@@ -15,7 +15,7 @@
 		<li class="footer-nav__link"><a href="${termsOfUseUrl}" class="btn">Соглашение</a></li>
 		<li class="footer-nav__link">
 			<a href="${lastUpdateUrl}" style="color: rgb(129, 212, 250); font-weight: bold" class="btn">
-				Последнее обновление: 02.01.2019
+				Последнее обновление: 10.01.2019
 			</a>
 		</li>
 	</ul>
